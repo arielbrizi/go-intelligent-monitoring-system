2858e936f6eea5b6434864a891057960a95396a2
Ariel <arielbrizi@gmail.com>
2020-11-19 23:09:57 -0300

    feat: add rotatelogs

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"go-intelligent-monitoring-system/utils"
-	"io"
-	"os"
 
 	logrus_stack "github.com/Gurpartap/logrus-stack"
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -14,10 +13,11 @@ func main() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetLevel(log.InfoLevel) // TODO: get from Config
 
-	var file, _ = os.OpenFile("logFile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	log.SetOutput(io.MultiWriter(file, os.Stdout))
-	log.AddHook(logrus_stack.StandardHook()) //Sets the time and code line on each log.
+	//One log file per day. Purge logs older than 7 days
+	rl, _ := rotatelogs.New("log.%Y%m%d", rotatelogs.WithMaxAge(-1), rotatelogs.WithRotationCount(7))
+	log.SetOutput(rl)
 
+	log.AddHook(logrus_stack.StandardHook()) //Sets the time and code line on each log.
 	log.Info("Initializing Intelligent Monitoring System")
 
 	// --------------- Configuration Port IN --------------- //
