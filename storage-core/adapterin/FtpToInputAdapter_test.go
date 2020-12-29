ca64a91b3f1a8266546fba9853ce4a05f693590d
Ariel <arielbrizi@gmail.com>
2020-12-29 00:08:39 -0300

    test: add FtpToInputAdapter test

--- storage-core/adapterin/FtpToInputAdapter_test.go
+++ storage-core/adapterin/FtpToInputAdapter_test.go
@@ -14,17 +14,17 @@ import (
 
 func TestProcess(test *testing.T) {
 
-	os.Setenv("FTP_DIRECTORY", "../../test/images/")
+	os.Setenv("FTP_DIRECTORY", "../../test/FtpToInputAdapter/")
 	os.Setenv("SCOPE", "test")
 
 	var t = time.Now()
 	var today = t.Format("20060102")
 
-	files, _ := ioutil.ReadDir("../../test/images/withFaces")
+	files, _ := ioutil.ReadDir("../../test/FtpToInputAdapter/20201228")
 	numberOfFiles := len(files)
 
-	//Rename "withFaces" to the pattern used by ftpToInputAdapter, for example: ../../test/images/20201227
-	os.Rename("../../test/images/withFaces", "../../test/images/"+today)
+	//Change folder name to 'today'
+	os.Rename("../../test/FtpToInputAdapter/20201228", "../../test/FtpToInputAdapter/"+today)
 
 	//Define the "Adapter Out" to be used to connect to the Image Queue core: Kafka
 	var queueAdapterOut storageapplicationportout.QueueImagePort
@@ -44,23 +44,20 @@ func TestProcess(test *testing.T) {
 	ftpToInputAdapter := NewFtpToInputAdapter(imageProcessingService, video2ImageService)
 	ftpToInputAdapter.Process()
 
-	//get the number of processed files
-	files, _ = ioutil.ReadDir("../../test/images/" + today + "_processed")
+	files, _ = ioutil.ReadDir("../../test/FtpToInputAdapter/" + today + "_processed")
 	if len(files) != numberOfFiles {
-		test.Errorf("Error processing files: shoud be %v and they are %v", numberOfFiles, len(files))
+		test.Errorf("Error processing files: shoud be %v and they are %v", len(files), numberOfFiles)
 	}
 
-	//rollback directories name
-	os.Rename("../../test/images/"+today+"_processed", "../../test/images/withFaces")
-	os.Remove("../../test/images/" + today)
-	os.Remove("../../test/images/" + today + "_faces_auth")
-	os.Remove("../../test/images/" + today + "_faces_not_auth")
-	os.Remove("../../test/images/" + today + "_processed")
+	os.Rename("../../test/FtpToInputAdapter/"+today+"_processed", "../../test/FtpToInputAdapter/20201228")
+	os.Remove("../../test/FtpToInputAdapter/" + today)
+	os.Remove("../../test/FtpToInputAdapter/" + today + "_faces_auth")
+	os.Remove("../../test/FtpToInputAdapter/" + today + "_faces_not_auth")
+	os.Remove("../../test/FtpToInputAdapter/" + today + "_processed")
 
-	//check if rollback is OK
-	files, _ = ioutil.ReadDir("../../test/images/withFaces")
+	files, _ = ioutil.ReadDir("../../test/FtpToInputAdapter/20201228")
 	if len(files) != numberOfFiles {
-		test.Errorf("Error processing files: shoud be %v and they are %v", numberOfFiles, len(files))
+		test.Errorf("Error processing files: shoud be %v and they are %v", len(files), numberOfFiles)
 	}
 
 }
